599651ab4876361eab836d2d9d5bee7e8d1b76a5
SaadVSP96 <saadmasood192@gmail.com>
2024-08-02 15:59:34 +0500

    Built Collapsible component

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector(".nav__list").classList.toggle("hidden");
         });
 });
+
+// Collapsibles Code:
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+    item.addEventListener("click", function () {
+        this.classList.toggle("collapsible--expanded");
+    })
+);
